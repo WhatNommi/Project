ff5a64487617665ddb56d63beac3ac04ac7ee522
B00055 <chungyenlai@gmail.com>
2018-07-04 11:54:59 +0800

    code review

--- app/src/main/java/com/example/nommi/myapplication/MainActivity.java
+++ app/src/main/java/com/example/nommi/myapplication/MainActivity.java
@@ -2,80 +2,43 @@ package com.example.nommi.myapplication;
 
 import android.content.res.Resources;
 import android.content.res.TypedArray;
-import android.graphics.Color;
 import android.os.CountDownTimer;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.app.ListActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
+import android.util.SparseArray;
 import android.widget.CompoundButton;
-import android.widget.LinearLayout;
-import android.widget.Toast;
 import android.widget.ToggleButton;
-import android.widget.ArrayAdapter;
-
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
+    private SparseArray<Integer>mypair=new SparseArray();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ToggleButton b1 = (ToggleButton) findViewById(R.id.btn);
+        ToggleButton b1 = findViewById(R.id.btn);
         Resources res = getResources();
+        for (int j=0,i=R.drawable.darkgray2;i<=R.drawable.darkgray9;j++,i++)
+            mypair.put(j,i);
         final TypedArray Mycolors = res.obtainTypedArray(R.array.rainbow);
         final Random random = new Random(); // create new
         final CountDownTimer waitTimer = new CountDownTimer(300000, 1000) {
             public void onTick(long millisUntilFinished) {
-                    int randomInt = 0;
-                    int drawableID = 0;
-                    randomInt = random.nextInt(Mycolors.length());
-                    drawableID = Mycolors.getResourceId(randomInt, -1);
+                    Integer randomInt = random.nextInt(Mycolors.length());
                     ConstraintLayout background = findViewById(R.id.back);
-                    //background.setBackgroundResource(drawableID);
-                    Toast.makeText(MainActivity.this, "run1 " + randomInt, Toast.LENGTH_SHORT).show();
-                    //System.out.println("Show mw");
-                    switch (randomInt){
-                        case 0:
-                            background.setBackgroundResource(R.drawable.ic_launcher_foreground);
-                        case 1:
-                            background.setBackgroundResource(R.drawable.darkgray2);
-                            break;
-                        case 2:
-                            background.setBackgroundResource(R.drawable.darkgray3);
-                            break;
-                        case 3:
-                            background.setBackgroundResource(R.drawable.darkgray4);
-                            break;
-                        case 4:
-                            background.setBackgroundResource(R.drawable.darkgray5);
-                            break;
-                        case 5:
-                            background.setBackgroundResource(R.drawable.darkgray6);
-                            break;
-                        case 6:
-                            background.setBackgroundResource(R.drawable.darkgray7);
-                            break;
-                        case 7:
-                            background.setBackgroundResource(R.drawable.darkgray8);
-                            break;
-                        case 8:
-                            background.setBackgroundResource(R.drawable.darkgray9);
-                            break;
-                    }
+                Log.d("randomInt",randomInt.toString());
+                background.setBackgroundResource(mypair.get(randomInt));
 
             }
             public void onFinish() {
-                    int randomInt = 0;
-                    int drawableID = 0;
-                    randomInt = random.nextInt(Mycolors.length());
-                    drawableID = Mycolors.getResourceId(randomInt, -1);
-                    ConstraintLayout background = findViewById(R.id.back);
-                    //background.setBackgroundResource(drawableID);
+//                    int randomInt = 0;
+//                    int drawableID = 0;
+//                    randomInt = random.nextInt(Mycolors.length());
+//                    drawableID = Mycolors.getResourceId(randomInt, -1);
+//                    ConstraintLayout background = findViewById(R.id.back);
+//                    //background.setBackgroundResource(drawableID);
             }
         };
         b1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
